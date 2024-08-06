a6e074d30e7e4f86587ffd729e642da7947dc38c
codinBabe <teboola2@gmail.com>
2024-08-06 13:49:55 +0100

    0-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    """
+    Generate a Pascal's Triangle of 'n' rows.
+
+    Args:
+    n (int): The number of rows in the triangle.
+
+    Returns:
+    list: A list of lists representing the Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
