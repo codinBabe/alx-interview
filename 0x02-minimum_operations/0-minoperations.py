f300606c684b3b0e990237319e004784aeb510ec
codinBabe <teboola2@gmail.com>
2024-08-20 20:06:18 +0100

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Minimum number of operations"""
+
+
+def minOperations(n):
+    """
+    Function to calculate minimum number of operations
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
+    
