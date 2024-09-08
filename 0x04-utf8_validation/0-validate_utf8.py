c1d598bb7cfef0e056d02ca1bb2b018f60746f80
codinBabe <teboola2@gmail.com>
2024-09-08 20:52:54 +0100

    fix:check fail

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,9 +3,22 @@
 
 def validUTF8(data):
     """check if a set of data is a valid utf-8"""
-    for i in range(len(data)):
-        num = data[i]
-        if chr(num):
-            return True
+    n_byte = 0
+    for byte in data:
+        if n_byte == 0:
+            if byte & 0b11111000 == 0b11110000:
+                n_byte = 3
+            elif byte & 0b11110000 == 0b11100000:
+                n_byte = 2
+            elif byte & 0b11100000 == 0b11000000:
+                n_byte = 1
+            elif byte & 0b10000000 == 0:
+                n_byte = 0
+            else:
+                return False
         else:
-            return False
+            if byte & 0b11000000 != 0b10000000:
+                return False
+            n_byte -= 1
+
+    return n_byte == 0
