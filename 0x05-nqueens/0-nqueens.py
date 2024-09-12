283a49f36f1006d111432f9b0d4343f9f2583882
codinBabe <teboola2@gmail.com>
2024-09-12 22:06:01 +0100

    completed

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""N queens problem"""
+
+
+import sys
+
+
+def isSafe(board, row, col, n):
+    """Check if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solveNQUtil(board, col, n):
+    """Solve N queens problem using backtracking"""
+    if col == n:
+        printBoard(board, n)
+        return True
+    res = False
+    for i in range(n):
+        if isSafe(board, i, col, n):
+            board[i][col] = 1
+            res = solveNQUtil(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
+
+
+def printBoard(board, n):
+    """Print the board"""
+    res = []
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                res.append([i, j])
+    print(res)
+
+
+def nqueens(n):
+    """Initialize the board and call solveNQUtil"""
+    board = [[0 for j in range(n)] for i in range(n)]
+    if solveNQUtil(board, 0, n) is False:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    nqueens(n)
