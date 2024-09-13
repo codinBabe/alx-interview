910466ba8295a7ed45a53ebf7b27ca7b64d3b5ed
codinBabe <teboola2@gmail.com>
2024-09-13 05:09:49 +0100

    completed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-""" N queens """
+"""N queens problem"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 n = int(sys.argv[1])
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+    """N queens problem"""
     if i < n:
         for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+            if j not in a and i+j not in b and i-j not in c:
+                for solution in queens(n, i+1, a+[j], b+[i+j], c+[i-j]):
+                    yield solution
     else:
         yield a
 
 
 def solve(n):
-    """ solve """
+    """Solve N queens problem"""
     k = []
     i = 0
     for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
+        for j in solution:
+            k.append([i, j])
             i += 1
         print(k)
         k = []
