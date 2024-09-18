016e8d94f269db1edfffdfcbda37e2d04d4b36d6
codinBabe <teboola2@gmail.com>
2024-09-18 09:11:15 +0100

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,33 @@
 #!/usr/bin/node
 // This script displays the names of characters from a Star Wars movie
 // based on the movie number given in the command line argument
-const request = require('request');
+const request = require("request");
 
-function getStarWarsCharacters () {
+function getStarWarsCharacters() {
   const arg = process.argv[2];
   if (arg && arg > 0 && arg <= 7) {
-    try {
-      request
-        .get(`https://swapi-api.alx-tools.com/api/films/${arg}`)
-        .on('response', (response) => {
-          const data = JSON.parse(response.body);
-          data.characters.forEach((character) => {
-            request.get(character).on('response', (response) => {
-              const data = JSON.parse(response.body);
-              console.log(data.name);
+    const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
+    request(url, async function (error, response, body) {
+      if (error) {
+        console.error(error);
+      } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+          const name = await new Promise((resolve, reject) => {
+            request(character, function (error, response, body) {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(JSON.parse(body).name);
+              }
             });
           });
-        });
-    } catch (error) {
-      console.log(error);
-    }
+          console.log(name);
+        }
+      }
+    });
   } else {
-    console.log('Enter a valid number between 1 and 7');
+    console.log("Enter a valid movie number between 1 and 7");
   }
 }
 
