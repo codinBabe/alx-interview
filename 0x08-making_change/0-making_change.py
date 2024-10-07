5442595f171690e38b62f13c1c0b809a27c7296b
codinBabe <teboola2@gmail.com>
2024-10-07 05:44:36 +0100

    completed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Making change alogrithim"""
+
+"""makeChange function"""
 
 
 def makeChange(coins, total):
-    """Making change alogrithim"""
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
     if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
-    coins_needed = 0
+    change = 0
     coins = sorted(coins)[::-1]
     for coin in coins:
-        while coins <= total:
+        while coin <= total:
             total -= coin
-            coins_needed += 1
-        if total == 0:
-            return coins_needed
+            change += 1
+        if (total == 0):
+            return change
     return -1
-
