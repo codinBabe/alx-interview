7b0a0e6ca0d2d72c8986c9124e58d93637480c40
codinBabe <teboola2@gmail.com>
2024-10-17 11:24:50 +0100

    feat:completed task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,41 +2,30 @@
 """A prime game module."""
 
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def rem_multiples(a, b):
+    """remove multiples of prime number."""
+    for i in range(2, len(a)):
+        try:
+            a[i * b] = 0
+        except (ValueError, IndexError):
+            break
 
 
 def isWinner(x, nums):
-    """Determine the winner of the game."""
-    if x < 1:
+    """Determine who the winner of the game is."""
+    if x < 1 or not nums:
         return None
-    if not nums:
-        return None
+
     n = max(nums)
-    primes = [0] * (n + 1)
-    for i in range(1, n + 1):
-        primes[i] = primes[i - 1]
-        if is_prime(i):
-            primes[i] += 1
-    wins = [0, 0]
-    for i in range(x):
-        score = primes[nums[i]] - (i - wins[0])
-        if score % 2 == 0:
-            wins[1] += 1
-        else:
-            wins[0] += 1
-    if wins[0] == wins[1]:
-        return None
-    return "Maria" if wins[0] > wins[1] else "Ben"
+    primes = [0, 0] + [1] * (n - 1)
+    for i in range(2, n + 1):
+        if primes[i]:
+            rem_multiples(primes, i)
+
+    primes = [i for i, n in enumerate(primes) if n]
+
+    m = 0
+    for n in nums:
+        m += sum(1 for p in primes if p <= n)
+
+    return "Maria" if m % 2 else "Ben"
